a51aad0815ffe91157e1a479d5182cfb2a26e373
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-04-05 17:04:20 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    console.log("render");
+    const fetchData = async () => {
+      const result = await axios.get("https://picsum.photos/v2/list");
+      setData(result);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
